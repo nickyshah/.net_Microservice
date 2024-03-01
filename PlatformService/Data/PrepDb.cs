299c67144ab2fb9ab17a52c14bf4b739d0e2621c
Nicky Shah <nickshah534@gmail.com>
2024-03-01 11:13:33 +1100

    Add project files.

---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,48 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding Data <--");
+
+                context.Platforms.AddRange(
+                    new Platform()
+                    {
+                        Name = "Dot Net",
+                        Publisher = "Microsoft",
+                        Cost = "Free"
+                    },
+                    new Platform()
+                    {
+                        Name = "Dot Net2",
+                        Publisher = "Microsoft2",
+                        Cost = "Free2"
+                    },
+                    new Platform()
+                    {
+                        Name = "Dot Net3",
+                        Publisher = "Microsoft3",
+                        Cost = "Free3"
+                    });
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> We already have the data <--");
+            }
+        }
+    }
+}
